0fddaffb1fac8aeae951d27b5d50801826e09394
40823149 <40823149@mde.tw>
2019-10-21 19:08:48 +0800

    initial add

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'USB \n http://mde.tw/cp2019 \xa0 \n \n \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': "USB \n http://mde.tw/cp2019 \xa0 \n void main() {\n  for (int i = 0; i < 5; i++) {\n    print('hello ${i + 1}');\n  }\n  print('hello 中文');\n}\n \n Gist: https://gist.github.com/40823149/a8a234b03afcbd3fc0e63845fa351dab \n Dart: http://mde.tw/dartpad/?id=a8a234b03afcbd3fc0e63845fa351dab \n \n", 'tags': '', 'url': 'About.html'}]};
