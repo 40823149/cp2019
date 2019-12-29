efcf3ec94db0bed6870680c8e0479e6abb56323e
40823149 <40823149@gm.nfu.edu.tw>
2019-12-29 12:20:36 +0800

    溫度轉換按鈕測試 2

---
+++ downloads/dart_ex/溫度轉換/溫度轉換.dart
@@ -0,0 +1,19 @@
+//輸入溫度
+List candf = ["50C", "80C", "40F", "30C", "23F"];
+
+void main() {
+  for (var i in candf) {
+    if (i[2] == "C") {
+      var o = (i[0] + i[1]);
+      var c = num.parse(o);
+      num f = c * 9 / 5 + 32;
+
+      print("C $c °等於F $f °");}
+    if (i[2] == "F") {
+      var o = (i[0] + i[1]);
+      var f = num.parse(o);
+      num c = (f - 32) * 5 / 9;
+      print("F $f °等於C $c ° ");
+    }
+  }
+}
