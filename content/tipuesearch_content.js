154333ee295376d112dc4270dbbfe9292afe162d
40823149 <40823149@gm.nfu.edu.tw>
2019-12-29 12:06:57 +0800

    整理3

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': '溫度轉換', 'text': '\n \n \n \n \n \n  取 Dart 程式的按鈕  \n Dart 程式 \n 華氏轉攝氏 \n 華攝氏轉換 \n  內建放入的 Dart 原始碼  \n 放大   回復', 'tags': '', 'url': '溫度轉換.html'}]};
+var tipuesearch = {"pages": [{'title': '溫度轉換', 'text': '\n \n \n \n \n \n  取 Dart 程式的按鈕  \n Dart 程式 \n 華氏轉攝氏 \n 華攝氏轉換 \n  內建放入的 Dart 原始碼  \n 放大   回復 \n', 'tags': '', 'url': '溫度轉換.html'}, {'title': 'About', 'text': '\n \n', 'tags': '', 'url': 'About.html'}]};
